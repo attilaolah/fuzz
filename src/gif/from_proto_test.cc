917c83ad4d754ab632c25d6f052e9cfec75d024b
Attila Oláh <attilaolah@gmail.com>
2023-04-12 13:32:22 +0200

    Update the test file.
    Turns out GIMP encodes files slightly differently than giflib, causing
    test failures when comparing files byte-by-byte. But re-encoding the
    file using giflib solves the issue.

--- src/gif/from_proto_test.cc
+++ src/gif/from_proto_test.cc
@@ -1,4 +1,9 @@
 #include <algorithm>
+#include <fstream>
+#include <istream>
+#include <vector>
+
+#include <cstdlib>
 
 #include <google/protobuf/text_format.h>
 #include <gtest/gtest.h>
@@ -23,7 +28,19 @@ TEST(FromProtoTest, ReEncode) {
   EXPECT_EQ(new_gif, gif);
   // Do one more in a loop where we can print the index:
   for (size_t i = 0; i < std::min(new_gif.size(), gif.size()); i++) {
-    ASSERT_EQ(new_gif[i], gif[i]) << "At byte index: " << i;
+    EXPECT_EQ(new_gif[i], gif[i]) << "At byte index: " << i;
+  }
+
+  // To write the results into a temporary file (e.g. for inspection), run:
+  // bazel test --test_env=TEST_FILE_OUT=/path/to/result.gif //src/gif:from_proto_test
+  if (const char* env_var = std::getenv("TEST_FILE_OUT"); env_var != nullptr) {
+    const std::string file_path(env_var);
+    if (std::ofstream file(file_path, std::ios::binary); file) {
+      file.write(reinterpret_cast<const char*>(new_gif.data()), new_gif.size());
+      file.close();
+    } else {
+      std::cerr << "ERROR: could not open file " << file_path << " for writing." << std::endl;
+    }
   }
 }
 
